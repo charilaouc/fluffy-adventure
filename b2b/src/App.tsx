a1673d85fc14cd1af752c398b277aab2f15c61a7
charilaouc <charilaou.c@gmail.com>
2022-09-15 12:28:49 +0000

    icon tsx and import in app

--- b2b/src/App.tsx
+++ b2b/src/App.tsx
@@ -2,6 +2,8 @@ import { Refine } from "@pankod/refine-core";
 import routerProvider from "@pankod/refine-react-router-v6";
 import dataProvider from "@pankod/refine-simple-rest";
 
+import {PostIcon} from "icons";
+
 const App: React.FC = () => {
     return (
         <Refine
